309a83efa4a8ed1dc18317d5cb22f869e365b7f1
Usame Algan <usame33@hotmail.de>
2017-08-08 02:14:41 +0200

    fixed the demo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,16 @@
-kb = new Tutorial("nico<3", {
+mytutorial = new Tutorial("mytutorial", {
     steps: [
         {
-            highlight: ".first-step",
+            highlight: ".tut-step",
             title: "mytitle",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-            callback: {
-                fn: (ctx) => console.log(ctx),
-                once: true
-            }
-        },
-        {
-            highlight: ".second-step",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-            callback: (ctx) => console.log("Tutorial is bound to this context")
-        },
-        {
-            highlight: ".third-step",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-            callback: {
-                fn: () => console.log("Tada much support, very lean")
-            }
-        },
-        {
-            highlight: ".fourth-step",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-            action: {
-                template: '#test-advanced'
-            }
-        },
-        {
-            highlight: ".scroll-test",
             text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
-        }],
-    debug: true,
-    persistent: 1,
-    buttons: {next: "Weiter"},
-    autoplay: true,
-    progressbar: false
-});
-
-function sayHello() {
-    alert("yolo");
-}
-
-Tutorial.installCustomAction({
-    key: "dbl",
-    event: "dblclick"
+        },
+        {
+            highlight: ".prettyprint",
+            title: "title2",
+            text: "loremipsum"
+        }
+    ],
+    autoplay:true,
+    debug: true
 });
